f5ee58c38817be1c4667ce2cd4e09295f23af126
Devansh Goel <DG088702@abcsupply.com>
2025-06-24 14:47:38 -0500

    Add chart generation for test case results and include EDW-1138.csv data file

--- qa.py
+++ qa.py
@@ -1,1 +1,19 @@
-print("Hello")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the CSV file
+df = pd.read_csv('EDW-1138.csv')
+
+# Count the number of PASS and FAIL in the 'Result' column
+result_counts = df['Result'].value_counts()
+
+# Plot the results
+plt.figure(figsize=(6,4))
+result_counts[['PASS', 'FAIL']].plot(kind='bar', color=['green', 'red'])
+plt.title('Total Number of Passed and Failed Test Cases')
+plt.xlabel('Result')
+plt.ylabel('Count')
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.show()
+
